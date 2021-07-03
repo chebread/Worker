d882d5a76c7121b5ddc4158e938c3bd7e7e14dec
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-07-04 00:53:41 +0900

    7/4

--- VERSIONS.md
+++ VERSIONS.md
@@ -2,3 +2,7 @@
 
 ### v1.0.0
 Basics functions
+
+### v1.1.0
+1. Edit the Text..., Changed logs, fonts, new colors, size
+2. worker be more beautiful !
--- exec.py
+++ exec.py
@@ -1,14 +1,13 @@
 # 급히 쓰는 노트
-from typing import TextIO
-from clint.textui import *
 from tkinter import *
+from clint.textui import *
 
 root = Tk() # use to tkinter
 root.title("Worker")
 root.geometry("260x245")
 #root.configure(bg='black')
 def Input(root):
-    Text(root, fg="green", bg="purple", highlightbackground="purple").pack(fill="both", expand=True)
+    Text(root, font=13, fg="medium spring green", bg="medium purple", highlightbackground="medium purple").pack(fill="both", expand=True)
 def Play(root):
     Input(root)
 
